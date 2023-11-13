f667f66e7f4e9d3d0b0fa45ece5a6c234c7979c6
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2023-11-13 19:41:03 +0200

    add parcel

---
+++ src/js/cat-api.js
@@ -0,0 +1,8 @@
+// import axios from 'axios';
+const BASE_URL = 'https://api.thecatapi.com/v1';
+const API_KEY =
+  'live_Yy7eiFiKcvfhRTQ6mGY9ukQ7zQoyZgLCFw1Mfdp7iMlSKUQcZmv6LiNFLAu15R1n';
+
+// axios.defaults.headers.common['x-api-key'] = API_KEY;
+console.log(API_KEY);
+// axios.defaults.baseURL = BASE_URL;
