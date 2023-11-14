4eab96c67636e27468aeb0bd7ce97f3b0f9ea3fb
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2023-11-14 20:40:11 +0200

    add cat-api.js

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,16 +1,9 @@
-// import axios from 'axios';
-
-// axios.defaults.headers.common['x-api-key'] = API_KEY;
-
-// axios.defaults.baseURL = BASE_URL;
-const BASE_URL = 'https://api.thecatapi.com';
-const END_POINT = '/v1/breeds';
+const BASE_URL = 'https://api.thecatapi.com/v1';
 const API_KEY =
   'live_Yy7eiFiKcvfhRTQ6mGY9ukQ7zQoyZgLCFw1Mfdp7iMlSKUQcZmv6LiNFLAu15R1n';
-console.log(API_KEY);
 
 export function fetchBreeds() {
-  return fetch(`${BASE_URL}${END_POINT}?${API_KEY}`).then(res => {
+  return fetch(`${BASE_URL}/breeds?api_key=${API_KEY}`).then(res => {
     if (!res.ok) {
       throw new Error(res.status);
     }
@@ -18,12 +11,13 @@ export function fetchBreeds() {
   });
 }
 
-export function createOptionCat(res) {
-  const selectBreed = document.querySelector('.breed-select');
-  res.map(item => {
-    selectBreed.insertAdjacentHTML(
-      'beforeend',
-      `<option value=${item.id}> ${item.name} </option>`
-    );
+export function fetchCatByBreed(breedId) {
+  return fetch(
+    `${BASE_URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`
+  ).then(res => {
+    if (!res.ok) {
+      throw new Error(res.status);
+    }
+    return res.json();
   });
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,40 @@
-import { fetchBreeds, createOptionCat } from './cat-api.js';
+import Notiflix from 'notiflix';
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+const selectBreed = document.querySelector('.breed-select');
+const catCard = document.querySelector('.cat-info');
 
 fetchBreeds()
   .then(res => {
     createOptionCat(res);
   })
   .catch(console.log);
+
+function createOptionCat(res) {
+  let arrayOption = res.map(item => {
+    return `<option value=${item.id}> ${item.name} </option>`;
+  });
+  selectBreed.insertAdjacentHTML('beforeend', arrayOption.join(''));
+}
+
+function createMarup(item) {
+  return `
+  <div class=""cat-card>
+    <img src="${item.url}" alt="${item.breeds[0].name}" class="cat-img" />
+  
+  <p class="cat-name">${item.breeds[0].name}</p>
+  <p class="cat-description">${item.breeds[0].description}</p>
+  <p class="cat-temperament">${item.breeds[0].description}</p>
+  </div>
+  `;
+}
+
+selectBreed.addEventListener('click', () => {
+  const currentCat = selectBreed.value;
+  fetchCatByBreed(currentCat)
+    .then(res => {
+      console.dir(res[0]);
+      const card = createMarup(res[0]);
+      catCard.innerHTML = card;
+    })
+    .catch(console.log());
+});
